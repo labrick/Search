759774ece0933bc04fd914f4e54b97d5e4a4fb3e
LarryBrick <yananback@gmail.com>
2013-09-28 10:49:34 +0800

    注册方面还不能解决重复的问题

---
+++ smodel/views.py
@@ -0,0 +1,31 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.db.models import Q
+from smodel.models import Person
+
+def index(request):
+    first_name = request.GET.get('first_name','')
+    last_name = request.GET.get('last_name','')
+
+
+    if first_name and last_name:
+        qset = (
+            Q(first_name__icontains=first_name) | 
+            Q(last_name__icontains=last_name)
+        )
+        results = Person.objects.filter(qset).distinct()
+        if results:
+            #already have
+            return render_to_response('index.html',{'results':results[0]})
+        else:
+            #register
+            p = Person(first_name=first_name,last_name=last_name)
+            p.save()
+            return render_to_response('index.html',{'first_name':first_name,'last_name':last_name,})
+    else:
+        #not null
+        if not first_name:
+            error = 'first_name'
+        if not last_name:
+            error = 'last_name'
+        return render_to_response('index.html',{'error':error})
